52ea62d2d1eebf222acf8a0a122a77dfe2608983
Sébastien BRAMILLE <2752200+oktapodia@users.noreply.github.com>
2021-02-25 11:49:57 +0000

    Use auto-changelog

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const {getOctokit, context} = require('@actions/github');
 const core = require('@actions/core');
 const util = require('util');
 const execFile = util.promisify(require('child_process').execFile);
-const {generateReleaseNotes} = require('./generate-release-notes');
 
 async function run() {
 	try {
-		const {owner, repo} = context.repo;
+		const { owner, repo } = context.repo;
+		const {stdout: changelogStdout } = await execFile('auto-changelog', ['--package', '--stdout', '--unreleased-only', '--hide-credit']);
 
-		const releaseTemplate = core.getInput('template');
-		const commitTemplate = core.getInput('commit-template');
-		const exclude = core.getInput('exclude');
-		const dateFormat = core.getInput('date-format');
+		if (!changelogStdout) {
+			core.info('There is nothing to be done here. Exiting!');
+			return;
+		}
 
 		// Fetch tags from remote
 		await execFile('git', ['fetch', 'origin', '+refs/tags/*:refs/tags/*']);
@@ -32,22 +32,13 @@ async function run() {
 			core.info('Using pushed tag as reference: ' + pushedTag);
 		}
 
-		// Get range to generate diff
-		let range = tags[1] + '..' + pushedTag;
-		if (tags.length < 2) {
-			const {stdout: rootCommit} = await execFile('git', ['rev-list', '--max-parents=0', 'HEAD']);
-			range = rootCommit.trim('') + '..' + pushedTag;
-		}
-
-		core.info('Computed range: ' + range);
-
 		// Create a release with markdown content in body
 		const octokit = getOctokit(core.getInput('token'));
 		const createReleaseResponse = await octokit.repos.createRelease({
 			repo,
 			owner,
 			tag_name: pushedTag, // eslint-disable-line camelcase
-			body: await generateReleaseNotes({range, exclude, commitTemplate, releaseTemplate, dateFormat}),
+			body: changelogStdout,
 			draft: false,
 			prerelease: false
 		});
